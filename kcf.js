c7ce6fb1f447f3d13f991da5c4331dc929022613
FeMiqui <51029840+FeMiqui@users.noreply.github.com>
2020-01-13 16:42:32 -0300

    Create kcf.js

---
+++ kcf.js
@@ -0,0 +1,13 @@
+//constant temperature of today's temperature in kelvin
+const kelvin = 0;
+
+//this is today's temperature in C
+var celsius = kelvin - 273;
+
+//this is today's temperature in F
+let fahrenheit = celsius * (9/5) + 32;
+
+//now we round the temperature in fahrenheit to the nearest integer
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
